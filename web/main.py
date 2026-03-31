0f87811bbec0f7248652350ebdd9b8ca45966526
TheStupidBear <dimasik.6.27i@gmail.com>
2026-03-31 14:12:47 +0300

    обхожу запреты post запросов

--- web/main.py
+++ web/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Form, Request
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from pathlib import Path
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from web import explorer, creature, user
+from web import explorer, creature, user, game
 from service import explorer as service_explorer
 from service import creature as service_creature
 
@@ -13,7 +14,21 @@ app = FastAPI()
 app.include_router(explorer.router)
 app.include_router(creature.router)
 app.include_router(user.router)
-# app.include_router(game.router)
+app.include_router(game.router)
+
+#разрешения
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000/"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 parent_dir = Path(__file__).resolve().parent.parent
 template_obj = Jinja2Templates(directory=f"{parent_dir}/template")
@@ -42,4 +57,4 @@ app.mount("/static",
  name="free")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000,reload=True)
