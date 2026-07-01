a8cc52f060d7f23e3d11db0cdd754e1d0c8ed8e1
TheStupidBear <dimasik.6.27i@gmail.com>
2026-07-01 11:42:55 +0300

    работает логин и возвращает токен

--- web/user.py
+++ web/user.py
@@ -1,13 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Form
+from pathlib import Path
+from fastapi.templating import Jinja2Templates
+from typing import Annotated
 from service import user as user_service
 from model.user import User
 
 router = APIRouter(prefix="/user")
 
+parent_dir = Path(__file__).resolve().parent.parent
+template_obj = Jinja2Templates(directory=f"{parent_dir}/template")
+
+#главная страница
+@router.post("/login")
+async def top(request: Request):
+    return template_obj.TemplateResponse("login.html",
+                                         {"request": request})
+
+
 @router.post("/get_token")
-async def login(user: User):
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
     #получаем токен и возращаем клиенту
-    token = user_service.login_for_access_token(user.username, user.password)
+    token = user_service.login_for_access_token(username, password)
     return token
 
 @router.post("/me")
